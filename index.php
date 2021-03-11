96483c8115b27fc95d73855161d13ed7c1f89f37
Marlon Santana dos Santos <marlonfms2012@gmail.com>
2021-03-11 17:16:42 -0300

    mudança para php
    mudança na extensão dos arquivos par .php para ser possível utilizar o include

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<?php include('util/commonHead.php'); ?>
+    <title>Párimeiker — Ferramenta de promoção e gerenciamento para eventos</title>
+</head>
+<body>
+    <header></header>
+    <main>
+        <h1>Seja Bem Vindo!</h1>
+        <a href="">Login</a>
+        <a href="">Cadastrar</a>
+    </main>
+    <footer>
+        <a href="">Sobre nós</a>
+    </footer>
+</body>
+</html>
