e7a08b8faf2dcfe9b1c3101e39eb26555e14d25a
Marlon Santana dos Santos <marlonfms2012@gmail.com>
2021-04-17 16:09:22 -0300

    pesquisa.php

--- view/pesquisa.php
+++ view/pesquisa.php
@@ -1,28 +1,39 @@
 <html>
     <head>
-    <?php include'../util/commonHead.php'; ?>
-    <meta charset="UTF-8">
+        <?php include("../util/commonHead.php"); ?>
+        <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Párimeiker — Ferramenta de promoção e gerenciamento para eventos</title>
     <?php 
-            session_start();
+           /* session_start();
             //caso por algum motivo o usuário acesse as páginas que requeram autenticação
             //sem se logar ele será redirecionado para a index
             if($_SESSION['logado'] != true){
                 header("location: /parimeikerWeb/index.php");
             }
-            $id = $_SESSION['id'];
+            $id = $_SESSION['id']; */
         ?>
     </head>
     <body>
-    <header>
-                <?php include('../util/header.php'); ?>
+        <header>
+            <?php include("../util/header.php"); ?>
         </header>
-        
-
         <div id="vazio"></div>
-        <h1>Olá <?php echo $_SESSION['nome']; ?></h1>
+        <main>
+            <div>
+                <h1>busca por nome</h1>
+            </div>    
+            <div>
+                <h1>busca por tag de empresa</h1>
 
+            </div>
+            <div>
+                <h1>busca por tag de autonomo</h1>
+            </div>
+        </main>
+        <footer>
+        
+        </footer>
     </body>
 </html>
