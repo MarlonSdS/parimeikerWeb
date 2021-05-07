9a3e00a55f9a8e5c4d298a9d7f529d59da8ab4e7
Marlon Santana dos Santos <marlonfms2012@gmail.com>
2021-05-07 15:30:15 -0300

    inicio da homepage

--- view/homepage.php
+++ view/homepage.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/styles/homepage.css">
     <title>Párimeiker — Ferramenta de promoção e gerenciamento para eventos</title>
     <?php 
             session_start();
@@ -22,7 +23,40 @@
         
 
         <div id="vazio"></div>
-        <h1>Olá <?php echo $_SESSION['nome']; ?></h1>
+        <main>
+        <h1>Novos colaboradores</h1>
+            <?php include("../controller/conexao.php");
+                //pesquisa por empresas
+                $result_nomes = "SELECT * FROM empresa WHERE id > 0 ORDER BY id DESC LIMIT 3";
+                $resultado_nomes = mysqli_query($conexao, $result_nomes);
+                
+                while($rows_nomes = mysqli_fetch_array($resultado_nomes)){
+                    $nome = $rows_nomes['nome'];
+                    $id = $rows_nomes['id'];
+                    ?>
+                <div class="newUsers">
+                    <img src="../userData/userProfilePictures/Empresa/userProfile<?php echo $id; ?>.png" alt="">
+                    <p><?php echo $nome; ?></p>
+                    <br>
+                </div>
+                 <?php } ?>
 
+                 <?php include("../controller/conexao.php");
+                //pesquisa por empresas
+                $result_nomes = "SELECT * FROM autonomo WHERE id > 0 ORDER BY id DESC LIMIT 3";
+                $resultado_nomes = mysqli_query($conexao, $result_nomes);
+                
+                while($rows_nomes = mysqli_fetch_array($resultado_nomes)){
+                    $nome = $rows_nomes['nome'];
+                    $id = $rows_nomes['id'];
+                    ?>
+                <div class="newUsers">
+                    <img src="../userData/userProfilePictures/Autonomo/userProfile<?php echo $id; ?>.png" alt="">
+                    <p><?php echo $nome; ?></p>
+                    <br>
+                </div>
+                 <?php } ?>
+        </main>
+        <footer></footer>
     </body>
 </html>
